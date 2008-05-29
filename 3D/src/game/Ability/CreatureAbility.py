5d34d1897d0b92f447aa4af18928b7ea4cbe0802
Incantus <incantus1@gmail.com>
2008-05-29 17:25:39 +0000

    Minor refactoring

--- 3D/src/game/Ability/CreatureAbility.py
+++ 3D/src/game/Ability/CreatureAbility.py
@@ -124,23 +124,26 @@ def berserker(subrole):
     return override(subrole, "setBlocked", setBlocked, keyword="berserker", combiner=logical_or)
 
 # 502.68b If a permanent has multiple instances of lifelink, each triggers separately.
-# XXX This is broken with trample, since the trigger only has the last amount of damage done
+# XXX Lifelink is broken when you have to split the damage
 def lifelink(subrole, card, in_play=False):
     from Ability import Ability
     from Effect import ChangeLife
     from TriggeredAbility import TriggeredAbility
     from Trigger import DealDamageTrigger
     subrole.keywords.add("lifelink")
+    # XXX This won't work because for a card with lifelink, the subrole won't have been assigned to a card
+    # XXX Fix this when i move abilities to the card
+    #card = subrole.card
     trigger = DealDamageTrigger(sender=card)
-    lifelink = TriggeredAbility(card, trigger = trigger,
+    life_link = TriggeredAbility(card, trigger = trigger,
             match_condition = lambda sender: True,
             ability = Ability(card, effects=ChangeLife(lambda life, t=trigger: life+t.amount)))
-    subrole.triggered_abilities.append(lifelink)
-    if in_play: lifelink.enteringPlay()
+    subrole.triggered_abilities.append(life_link)
+    if in_play: life_link.enteringPlay()
     def remove_lifelink():
         subrole.keywords.remove("lifelink")
-        lifelink.leavingPlay()
-        subrole.triggered_abilities.remove(lifelink)
+        life_link.leavingPlay()
+        subrole.triggered_abilities.remove(life_link)
     return remove_lifelink
 
 def fear(subrole):
@@ -225,7 +228,7 @@ def prevent_damage(subrole, amt, txt=None, condition=None, next=True):
 def regenerate(subrole, txt="Regenerate", condition=None):
     def canDestroy(self):
         if self.perm.canBeTapped(): self.perm.tap()
-        if isCreature(self.perm.card):
+        if isCreature(self.card):
             self.clearDamage()
             self.clearCombatState()
         # expire it
