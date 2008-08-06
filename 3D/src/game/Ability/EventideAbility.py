d35d3ec399636dacec6098c00c4662743e8037b6
Incantus <incantus1@gmail.com>
2008-08-06 16:38:51 +0000

    fixing retrace

--- 3D/src/game/Ability/EventideAbility.py
+++ 3D/src/game/Ability/EventideAbility.py
@@ -5,12 +5,12 @@ from game.Cost import DiscardCost
 def retrace(card):
     card.keywords.add("retrace")
 
-    main_spell = out_play_role.play_spell
+    main_spell = card.play_spell
     cost = main_spell.cost + DiscardCost(cardtype=isLandType)
 
     retrace = main_spell.__class__(card, cost, target=main_spell.targets, effects=main_spell.effects, copy_targets=main_spell.copy_targets, limit=main_spell.limit, zone="graveyard")
 
-    remove_ability = card.add(retrace)
+    remove_ability = card.abilities.add(retrace)
     def remove():
         card.keywords.remove("retrace")
         remove_ability()
