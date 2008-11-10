3894a4295f3d498c21f3c783c98aef50cd0a2c8d
Incantus <incantus1@gmail.com>
2008-11-10 19:09:06 +0000

    MageKing17 - a proper cost for cycling

--- 3D/src/game/Ability/CyclingAbility.py
+++ 3D/src/game/Ability/CyclingAbility.py
@@ -1,29 +1,37 @@
-from Cost import ManaCost, DiscardCost
+from Cost import ManaCost, Cost
 from ActivatedAbility import ActivatedAbility
 from game.Match import isCard
-from game.GameEvent import CardCycledEvent
+from game.GameEvent import DiscardCardEvent, CardCycledEvent
 from Target import NoTarget
+
+# A bit of a hack, but neccessary to make sure the event gets sent at the right time.
+
+class CycleDiscard(Cost):
+    def precompute(self, source, player): return True
+    def compute(self, source, player): return str(source.zone) == "hand"
+    def pay(self, source, player):
+        self.payment = player.discard(source)
+        if self.payment: self.payment.send(CardCycledEvent())
+    def __str__(self): return "Discard this card"
 
 def cycling(cost):
     if type(cost) == str: cost = ManaCost(cost)
     def effects(controller, source):
-        yield cost + DiscardCost()
+        yield cost + CycleDiscard()
         yield NoTarget()
         controller.draw()
-        source.send(CardCycledEvent())
         yield
-    return ActivatedAbility(effects, None, "hand", "Cycling %s"%str(cost))
+    return ActivatedAbility(effects, None, "hand", txt="Cycling %s"%str(cost), keyword="cycling")
 
 def typecycling(subtype, cost):
     if type(cost) == str: cost = ManaCost(cost)
     def effects(controller, source):
-        yield cost + DiscardCost()
+        yield cost + CycleDiscard()
         yield NoTarget()
         for card in controller.choose_from_zone(number=1, cardtype=isCard.with_condition(lambda c: c.subtypes == subtype), zone="library", action=subtype, required=False):
             card.move_to(card.owner.hand)
-            source.send(CardCycledEvent())
         yield
-    return ActivatedAbility(effects, None, "hand", "%scycling %s"%(subtype, str(cost)))
+    return ActivatedAbility(effects, None, "hand", txt="%scycling %s"%(subtype, str(cost)), keyword="cycling")
 
 plains_cycling = lambda cost: typecycling("Plains", cost)
 island_cycling = lambda cost: typecycling("Island", cost)
