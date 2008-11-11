d4746371e635df020ff7943e89f8192e48ff449e
Incantus <incantus1@gmail.com>
2008-11-11 15:02:57 +0000

    Removed double definition of keyword_action decorator - also renamed self to player to reflect original implementation

--- 3D/src/game/Ability/FifthDawnAbility.py
+++ 3D/src/game/Ability/FifthDawnAbility.py
@@ -1,13 +1,10 @@
 from EffectsUtilities import keyword_action
 
 @keyword_action
-def scry(self, number):
-    cards = self.library.top(number)
-    bottom_cards = set(self.getCardSelection(cards, number=-1, prompt="Pick any number of cards to put on the bottom of your library."))
+def scry(player, number):
+    cards = player.library.top(number)
+    bottom_cards = set(player.getCardSelection(cards, number=-1, prompt="Pick any number of cards to put on the bottom of your library."))
     for card in cards:
         if card in bottom_cards: position = 'bottom'
         else: position = 'top'
-        card.move_to(self.library, position=position)
-
-def keyword_action(func):
-    setattr(Player, func.__name__, func)
+        card.move_to(player.library, position=position)
