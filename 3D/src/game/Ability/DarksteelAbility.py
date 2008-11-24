de79db04c683f62ed89dffdddfe40144a7dcbea1
Incantus <incantus1@gmail.com>
2008-11-24 23:09:33 +0000

    Incorrect argument name for Target in modular

--- 3D/src/game/Ability/DarksteelAbility.py
+++ 3D/src/game/Ability/DarksteelAbility.py
@@ -15,7 +15,7 @@ def modular(n):
     cip = CiPAbility(effects, txt=txt, keyword="modular")
 
     def effects(controller, source):
-        target = yield Target(isArtifactCreature, txt="Artifact Creature")
+        target = yield Target(isArtifactCreature, msg="Target Artifact Creature for %s"%source)
         if controller.you_may("move all +1/+1 counters from %s to target"%source.name):
             target.add_counters(PowerToughnessCounter(1,1), number=source.num_counters("+1+1"))
         yield
