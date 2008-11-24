7af125793b54b967bba5123286a5a3cb6d02557e
Incantus <incantus1@gmail.com>
2008-11-24 19:57:34 +0000

    use Shift-L to show log

--- 3D/src/ui/Incantus.py
+++ 3D/src/ui/Incantus.py
@@ -244,7 +244,7 @@ class GameWindow(window.Window):
             self.user_action = game.Action.PassPriority()
         elif symbol == key.ESCAPE:
             self.user_action = game.Action.CancelAction()
-        elif symbol == key.TAB:
+        elif symbol == key.L and modifiers & key.MOD_SHIFT:
             self.game_status.toggle_gamelog()
         elif symbol == key.Q:
             self.has_exit=True
