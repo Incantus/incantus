6d6a167baa913bcd175521cf7b08e3f9796ae9fb
Incantus <incantus1@gmail.com>
2008-12-03 22:13:49 +0000

    Cleaned up LazyInt a bit

--- 3D/src/game/LazyInt.py
+++ 3D/src/game/LazyInt.py
@@ -1,20 +1,22 @@
 
 from pydispatch import dispatcher
-from GameEvent import HasPriorityEvent, TimestepEvent
 
+# Can't use properties, because that requires it derives from object and then __coerce__ won't work
 class LazyInt:
-    def __init__(self, func, finalize=False, event=HasPriorityEvent()): #TimestepEvent()):
+    def __init__(self, func, reset_event=None):
         if not callable(func): raise Exception("Argument to LazyInt init must be a function")
         self._func = func
-        self._final_value = None
-        self.finalize = finalize
-        def reset(self=self): self._final_value = None
-        if self.finalize: dispatcher.connect(reset, signal=event, weak=False)
-    def value(self):
-        if self.finalize:
-            if not self._final_value: self._final_value = self._func()
-            return self._final_value
-        else: return self._func()
+        if reset_event:
+            self.value = self.finalize_value
+            self.reset()
+            dispatcher.connect(self.reset, signal=reset_event)
+        else:
+            self.value = self.recomp_value
+    def reset(self): self._final_value = None
+    def recomp_value(self): return self._func()
+    def finalize_value(self):
+        if not self._final_value: self._final_value = self._func()
+        return self._final_value
     def __coerce__(self, other):
         return (self.value(), other)
     def __neg__(self):
