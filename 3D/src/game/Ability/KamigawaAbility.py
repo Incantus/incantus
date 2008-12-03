b0c0c823680405f98b815bd5772c4c89b3a15435
Incantus <incantus1@gmail.com>
2008-12-03 21:48:30 +0000

    Use LazyInt properly

--- 3D/src/game/Ability/KamigawaAbility.py
+++ 3D/src/game/Ability/KamigawaAbility.py
@@ -22,7 +22,6 @@ def bushido(value):
     else: txt = "Bushido X"
     def effects(controller, source):
         yield NoTarget()
-        if not type(value) == int: value = value()
         until_end_of_turn(source.augment_power_toughness(value, value))
         yield
     ability = TriggeredAbility([Trigger(BlockerDeclaredEvent()), Trigger(AttackerBlockedEvent())], sender_match, effects, zone="play", txt=txt, keyword='bushido')
