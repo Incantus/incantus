4828e2313fad5161810bfdfc7025c7768ce24c85
Incantus <incantus1@gmail.com>
2008-12-03 16:04:43 +0000

    Use LazyInt for bushido in the card code

--- 3D/src/game/Ability/KamigawaAbility.py
+++ 3D/src/game/Ability/KamigawaAbility.py
@@ -1,6 +1,5 @@
 from game.Match import isCard
 from game.GameEvent import BlockerDeclaredEvent, AttackerBlockedEvent
-from game.LazyInt import LazyInt
 from TriggeredAbility import TriggeredAbility, sender_match
 from Trigger import Trigger, EnterFromTrigger
 from Target import NoTarget
@@ -19,13 +18,11 @@ def soulshift(n):
             txt="Soulshift %s"%n)
 
 def bushido(value):
-    if callable(value): value = LazyInt(value)
-    txt="Bushido %d"%value
-    if isinstance(value, LazyInt): txt="Bushido X"
+    if isinstance(value, int): txt="Bushido %d"%value
+    else: txt = "Bushido X"
     def effects(controller, source):
         yield NoTarget()
-        temp = value if not isinstance(value, LazyInt) else value.value()
-        until_end_of_turn(source.augment_power_toughness(temp, temp))
+        until_end_of_turn(source.augment_power_toughness(value, value))
         yield
     ability = TriggeredAbility([Trigger(BlockerDeclaredEvent()), Trigger(AttackerBlockedEvent())], sender_match, effects, zone="play", txt=txt, keyword='bushido')
     ability.bushido_value = value
