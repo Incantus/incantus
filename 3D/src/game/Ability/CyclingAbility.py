1d2a0911a46b7e31446059176d75a13da45ab24f
Incantus <incantus1@gmail.com>
2009-02-17 04:26:01 +0000

    Incorporated MageKing's addition of basic land cycling

--- 3D/src/game/Ability/CyclingAbility.py
+++ 3D/src/game/Ability/CyclingAbility.py
@@ -1,7 +1,7 @@
 from Cost import ManaCost, Cost
 from ActivatedAbility import ActivatedAbility
-from game.Match import isCard
-from game.GameEvent import DiscardCardEvent, CardCycledEvent
+from game.Match import isCard, isBasicLandCard
+from game.GameEvent import DiscardCardEvent, CardCycledEvent, TimestepEvent
 from Target import NoTarget
 
 # A bit of a hack, but neccessary to make sure the event gets sent at the right time.
@@ -11,27 +11,32 @@ class CycleDiscard(Cost):
     def compute(self, source, player): return str(source.zone) == "hand"
     def pay(self, source, player):
         self.payment = player.discard(source)
+        #XXX This is a bit of a hack - i don't like sending the TimestepEvent here
+        player.send(TimestepEvent())
         if self.payment: self.payment.send(CardCycledEvent())
     def __str__(self): return "Discard this card"
 
 def cycling(cost):
-    if type(cost) == str: cost = ManaCost(cost)
     def effects(controller, source):
         yield cost + CycleDiscard()
         yield NoTarget()
         controller.draw()
         yield
-    return ActivatedAbility(effects, None, "hand", txt="Cycling %s"%str(cost), keyword="cycling")
+    return ActivatedAbility(effects, zone="hand", txt="Cycling %s"%str(cost), keyword="cycling")
 
-def typecycling(subtype, cost):
-    if type(cost) == str: cost = ManaCost(cost)
+def search_cycling(match, cost, typestr):
     def effects(controller, source):
         yield cost + CycleDiscard()
         yield NoTarget()
-        for card in controller.choose_from_zone(number=1, cardtype=isCard.with_condition(lambda c: c.subtypes == subtype), zone="library", action=subtype, required=False):
+        for card in controller.choose_from_zone(number=1, cardtype=match, zone="library", action="put into your hand"):
             card.move_to("hand")
         yield
-    return ActivatedAbility(effects, None, "hand", txt="%scycling %s"%(subtype, str(cost)), keyword="cycling")
+    return ActivatedAbility(effects, zone="hand", txt="%scycling %s"%(typestr, str(cost)), keyword="cycling")
+
+basic_landcycling = lambda cost: search_cycling(isBasicLandCard, cost, "Basic land")
+
+def typecycling(subtype, cost):
+    return search_cycling(match=isCard.with_condition(lambda c: c.subtypes == subtype), cost=cost, typestr=subtype)
 
 plains_cycling = lambda cost: typecycling("Plains", cost)
 island_cycling = lambda cost: typecycling("Island", cost)
