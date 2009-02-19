f89361353862add99090d20b0b298ab7aa4dad60
Incantus <incantus1@gmail.com>
2009-02-19 03:21:21 +0000

    reduce needs a starting value

--- engine/Ability/ConfluxAbility.py
+++ engine/Ability/ConfluxAbility.py
@@ -2,6 +2,6 @@ from engine.Match import isLand
 from Subtypes import all_basic_lands
 
 def domain(player):
-    landtypes = reduce(lambda s, l: s.union(l.subtypes.current), player.play.get(isLand))
+    landtypes = reduce(lambda s, l: s.union(l.subtypes.current), player.play.get(isLand), set())
     landtypes.intersection_update(all_basic_lands)
     return landtypes
