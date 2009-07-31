81f0d3cebb9ca31d3a47750b1beaf3556d035636
Incantus <incantus1@gmail.com>
2009-07-31 03:32:34 +0000

    Some cleanup for triggered abilities before being announced

--- engine/Stack.py
+++ engine/Stack.py
@@ -14,24 +14,23 @@ class Stack(Zone):
         cardtmpl = GameObject._cardmap[card.key]
         return cardtmpl.new_role(cardtmpl.stack_role)
     def add_triggered(self, ability, source):
-        self.pending_triggered.append(ability)
-        # XXX This is a bit ugly
-        ability.source = source
+        self.pending_triggered.append((ability, source))
     def process_triggered(self):
         # Check if there are any triggered abilities waiting
         if len(self.pending_triggered) > 0:
+            players = self.game.players
             # group all triggered abilities by player
-            triggered_sets = dict([(player, []) for player in self.game.players])
-            for ability in self.pending_triggered:
-                player = ability.source.controller
+            triggered_sets = dict([(player, []) for player in players])
+            for ability, source in self.pending_triggered:
+                player = source.controller
                 triggered_sets[player].append(ability)
             # Now ask the player to order them if there are more than one
-            for player in self.game.players:
+            for player in players:
                 triggered = triggered_sets[player]
                 if len(triggered) > 1:
                     triggered = player.make_selection(triggered, -1, prompt="Drag to reorder triggered abilities(Top ability resolves first)")
                 # Now reorder
-                for ability in triggered: ability.announce(ability.source, player)
+                for ability in triggered: ability.announce(source, player)
             self.pending_triggered[:] = []
             return True
         else: return False
