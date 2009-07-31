36a19d5a9af31883187a34dc797c1124d8abc2d2
Incantus <incantus1@gmail.com>
2009-07-31 03:31:49 +0000

    Removed buyback (for now) and also handles consolidated Stack

--- engine/Ability/CastingAbility.py
+++ engine/Ability/CastingAbility.py
@@ -8,7 +8,7 @@ class CastSpell(CostAbility):
         # Move the card to the stack zone - this is never called from play
         player = self.controller
         old_zone = self.source.zone
-        self.source = player.stack.put_card(self.source)
+        self.source = self.source.move_to(player.stack)
         if super(CastSpell, self).do_announce():
             return True
         else:
@@ -41,15 +41,3 @@ class CastNonPermanentSpell(CastSpell):
 
 class CastInstantSpell(CastNonPermanentSpell): pass
 class CastSorcerySpell(CastNonPermanentSpell): limit_type = sorcery
-
-#class CastBuybackSpell(CastSpell):
-#    def __str__(self):
-#        return "Buyback "+super(CastBuybackSpell, self).__str__()
-#
-## XXX This should really be a replacement effect, replacing going to your graveyard
-## This only matters if you play a spell that you don't own
-#def buyback(card, buyback="0"):
-#    main_spell = card.play_spell
-#    cost = main_spell.cost + buyback
-#
-#    card.abilities.add(CastBuybackSpell(out_play_role.card, cost, main_spell.targets, main_spell.effects, main_spell.copy_targets, main_spell.limit))
