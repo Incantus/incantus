03befff7137da1e490491eea6254e2b9f4b12628
Incantus <incantus1@gmail.com>
2009-12-19 04:58:10 +0000

    Changed modal decorator to be just an effects function, like the modal_triggered_effects function

--- engine/Ability/Decorators.py
+++ engine/Ability/Decorators.py
@@ -5,8 +5,6 @@ from CastingAbility import CastInstantSpell, CastSorcerySpell
 from Target import NoTarget, Target
 from Cost import ManaCost
 from CiPAbility import CiP, CiPAbility
-from EffectsUtilities import robustApply
-from Utils import flatten, unflatten
 
 # Decorators for effects of cards
 def sorcery(txt="Play sorcery"):
@@ -20,29 +18,6 @@ def instant(txt="Play instant"):
         effects = ability()
         return CastInstantSpell(effects, txt=txt)
     return make_spell
-
-def modal(*modes, **kw):
-    choose = kw['choose']
-    def make_modal(effects):
-        def modal_effects(controller, source):
-            selected = controller.make_selection([(mode.__doc__,mode) for mode in modes], choose, prompt='Select %d mode(s):'%choose)
-            if choose > 1: chosen = tuple((mode(controller, source) for mode in selected))
-            else: chosen = (selected(controller, source), )
-            # get the costs
-            # We need to have a payment = yield NoCost in the mode to pass
-            # back the cost in case the mode needs to reference is (see Profane Command)
-            costs = tuple((mode.next() for mode in chosen))
-            payment = yield effects(controller, source).next()
-
-            # get the targets - demultiplex them
-            targets = tuple((mode.send(payment) for mode in chosen))
-            demux = [len(target) if type(target) == tuple else 1 for target in targets]
-            targets = yield tuple(flatten(targets))
-            if not hasattr(targets, "__len__"): targets = (targets, )
-            yield tuple((mode.send(t) for t, mode in zip(tuple(unflatten(targets, demux)), chosen)))
-
-        return modal_effects
-    return make_modal
 
 def mana(limit=None, zone='play', txt=''):
     def make_ability(ability):
--- engine/Ability/GeneralAbility.py
+++ engine/Ability/GeneralAbility.py
@@ -2,6 +2,7 @@ from engine.CardRoles import Creature
 from engine.Planeswalker import Planeswalker
 from engine.Player import Player
 from EffectsUtilities import combine, do_replace
+from Utils import flatten, unflatten
 
 preventAll = lambda self, amt, source, combat=False: 0
 
@@ -24,3 +25,24 @@ def prevent_all_noncombat_damage():
     return combine(do_replace(Creature, "assignDamage", preventAll, msg=msg, condition=condition),
             do_replace(Player, "assignDamage", preventAll, msg=msg, condition=condition),
             do_replace(Planeswalker, "assignDamage", preventAll, msg=msg, condition=condition))
+
+def modal_effects(*modes, **kw):
+    choose = kw['choose']
+    def effects(controller, source):
+        selected = controller.make_selection([(mode.__doc__,mode) for mode in modes], choose, prompt='Select %d mode(s):'%choose)
+        if choose > 1: chosen = tuple((mode(controller, source) for mode in selected))
+        else: chosen = (selected(controller, source), )
+        # get the costs
+        # We need to have a "payment = yield NoCost" in the mode to pass
+        # back the cost in case the mode needs to reference is (see Profane Command)
+        empty_costs = tuple((mode.next() for mode in chosen))
+        payment = yield source.cost
+
+        # get the targets - demultiplex them
+        targets = tuple((mode.send(payment) for mode in chosen))
+        demux = [len(target) if type(target) == tuple else 1 for target in targets]
+        targets = yield tuple(flatten(targets))
+        if not hasattr(targets, "__len__"): targets = (targets, )
+        yield tuple((mode.send(t) for t, mode in zip(tuple(unflatten(targets, demux)), chosen))
+)
+    return effects
