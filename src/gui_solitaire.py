48f3766451702c9c239ff60478f6fe45df7881dd
Incantus <incantus1@gmail.com>
2010-01-10 14:12:00 -0500

    A little rearrangement in gui_solitaire - trying to consolidate with solitaire functionality in Incantus.py

--- src/gui_solitaire.py
+++ src/gui_solitaire.py
@@ -21,24 +21,25 @@ def on_exit():
 from ui.Incantus import IncantusLayer, load_deckfile
 from ui.resources import config
 
-def start_solitaire():
+def start_solitaire(player_name, players):
     gamescene = Scene()
     gamelayer = IncantusLayer()
+    gamescene.add(gamelayer, z=0, name="table")
+    gamelayer.game_start(player_name, pyglet.clock.time.time(), players)
+    return gamescene
+
+def main():
     p1_name = config.get("main", "playername")
     p2_name = config.get("solitaire", "playername")
     p1_deckfile = config.get("main", "deckfile")
     p2_deckfile = config.get("solitaire", "deckfile") 
     players = [(p1_name, load_deckfile(p1_deckfile)), (p2_name, load_deckfile(p2_deckfile))]
-    gamescene.add(gamelayer, z=0, name="table")
-    gamelayer.game_start(p1_name, pyglet.clock.time.time(), players, None)
-    return gamescene
 
-def main():
     pyglet.clock.schedule(lambda dt: reactor.resume())
     pyglet.clock.schedule(anim.add_time)
 
     director.init(resizable=True, width=1024, height=768)
-    director.run(start_solitaire())
+    director.run(start_solitaire(p1_name, players))
     #win.set_icon(pyglet.image.load("./data/Incantus.png"))
 
 if __name__ == '__main__':
