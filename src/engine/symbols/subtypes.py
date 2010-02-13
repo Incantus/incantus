5e90fd73a9cc62d2ba8cbd773c69582f1b0a2929
Incantus <incantus1@gmail.com>
2010-02-13 10:37:32 -0500

    New subtypes for Worldwake

--- src/engine/symbols/subtypes.py
+++ src/engine/symbols/subtypes.py
@@ -1,6 +1,7 @@
 from _symbols import Symbol
 
 Advisor = Symbol("Advisor", __name__)
+Ally = Symbol("Ally", __name__)
 Angel = Symbol("Angel", __name__)
 Anteater = Symbol("Anteater", __name__)
 Antelope = Symbol("Antelope", __name__)
@@ -217,7 +218,7 @@ Yeti = Symbol("Yeti", __name__)
 Zombie = Symbol("Zombie", __name__)
 Zubera = Symbol("Zubera", __name__)
 
-all_creatures = set([Advisor, Angel, Anteater, Antelope, Ape, Archer, Archon, Artificer, Assassin, AssemblyWorker, Atog, Auroch, Avatar, Badger, Barbarian, Basilisk, Bat, Bear, Beast, Beeble, Berserker, Bird, Blinkmoth, Boar, Bringer, Brushwagg, Camarid, Camel, Caribou, Carrier, Cat, Centaur, Cephalid, Chimera, Citizen, Cleric, Cockatrice, Construct, Coward, Crab, Crocodile, Cyclops, Dauthi, Demon, Deserter, Devil, Djinn, Dragon, Drake, Dreadnought, Drone, Druid, Dryad, Dwarf, Efreet, Egg, Elder, Elemental, Elephant, Elf, Elk, Eye, Faerie, Ferret, Fish, Flagbearer, Fox, Frog, Fungus, Gargoyle, Giant, Gnome, Goat, Goblin, Golem, Gorgon, Graveborn, Griffin, Hag, Harpy, Hellion, Hippo, Homarid, Homunculus, Horror, Horse, Hound, Human, Hydra, Hyena, Illusion, Imp, Incarnation, Insect, Jellyfish, Juggernaut, Kavu, Kirin, Kithkin, Knight, Kobold, Kor, Kraken, Lammasu, Leech, Leviathan, Lhurgoyf, Licid, Lizard, Manticore, Masticore, Mercenary, Merfolk, Metathran, Minion, Minotaur, Monger, Mongoose, Monk, Moonfolk, Mutant, Myr, Mystic, Nautilus, Nephilim, Nightmare, Nightstalker, Ninja, Noggle, Nomad, Octopus, Ogre, Ooze, Orb, Orc, Orgg, Ouphe, Ox, Oyster, Pegasus, Pentavite, Pest, Phelddagrif, Phoenix, Pincher, Pirate, Plant, Prism, Rabbit, Rat, Rebel, Reflection, Rhino, Rigger, Rogue, Salamander, Samurai, Sand, Saproling, Satyr, Scarecrow, Scorpion, Scout, Serf, Serpent, Shade, Shaman, Shapeshifter, Sheep, Skeleton, Slith, Sliver, Slug, Snake, Soldier, Soltari, Spawn, Specter, Spellshaper, Sphinx, Spider, Spike, Spirit, Splinter, Sponge, Squid, Squirrel, Starfish, Survivor, Tetravite, Thalakos, Thopter, Thrull, Treefolk, Triskelavite, Troll, Turtle, Unicorn, Vampire, Vedalken, Viashino, Volver, Wall, Warrior, Weird, Whale, Wizard, Wolf, Wolverine, Wombat, Worm, Wraith, Wurm, Yeti, Zombie, Zubera])
+all_creatures = set([Advisor, Ally, Angel, Anteater, Antelope, Ape, Archer, Archon, Artificer, Assassin, AssemblyWorker, Atog, Auroch, Avatar, Badger, Barbarian, Basilisk, Bat, Bear, Beast, Beeble, Berserker, Bird, Blinkmoth, Boar, Bringer, Brushwagg, Camarid, Camel, Caribou, Carrier, Cat, Centaur, Cephalid, Chimera, Citizen, Cleric, Cockatrice, Construct, Coward, Crab, Crocodile, Cyclops, Dauthi, Demon, Deserter, Devil, Djinn, Dragon, Drake, Dreadnought, Drone, Druid, Dryad, Dwarf, Efreet, Egg, Elder, Elemental, Elephant, Elf, Elk, Eye, Faerie, Ferret, Fish, Flagbearer, Fox, Frog, Fungus, Gargoyle, Giant, Gnome, Goat, Goblin, Golem, Gorgon, Graveborn, Griffin, Hag, Harpy, Hellion, Hippo, Homarid, Homunculus, Horror, Horse, Hound, Human, Hydra, Hyena, Illusion, Imp, Incarnation, Insect, Jellyfish, Juggernaut, Kavu, Kirin, Kithkin, Knight, Kobold, Kor, Kraken, Lammasu, Leech, Leviathan, Lhurgoyf, Licid, Lizard, Manticore, Masticore, Mercenary, Merfolk, Metathran, Minion, Minotaur, Monger, Mongoose, Monk, Moonfolk, Mutant, Myr, Mystic, Nautilus, Nephilim, Nightmare, Nightstalker, Ninja, Noggle, Nomad, Octopus, Ogre, Ooze, Orb, Orc, Orgg, Ouphe, Ox, Oyster, Pegasus, Pentavite, Pest, Phelddagrif, Phoenix, Pincher, Pirate, Plant, Prism, Rabbit, Rat, Rebel, Reflection, Rhino, Rigger, Rogue, Salamander, Samurai, Sand, Saproling, Satyr, Scarecrow, Scorpion, Scout, Serf, Serpent, Shade, Shaman, Shapeshifter, Sheep, Skeleton, Slith, Sliver, Slug, Snake, Soldier, Soltari, Spawn, Specter, Spellshaper, Sphinx, Spider, Spike, Spirit, Splinter, Sponge, Squid, Squirrel, Starfish, Survivor, Tetravite, Thalakos, Thopter, Thrull, Treefolk, Triskelavite, Troll, Turtle, Unicorn, Vampire, Vedalken, Viashino, Volver, Wall, Warrior, Weird, Whale, Wizard, Wolf, Wolverine, Wombat, Worm, Wraith, Wurm, Yeti, Zombie, Zubera])
 
 Contraption = Symbol("Contraption", __name__)
 Equipment = Symbol("Equipment", __name__)
@@ -249,7 +250,9 @@ Elspeth = Symbol("Elspeth", __name__)
 Garruk = Symbol("Garruk", __name__)
 Jace = Symbol("Jace", __name__)
 Liliana = Symbol("Liliana", __name__)
+Nissa = Symbol("Nissa", __name__)
 Sarkhan = Symbol("Sarkhan", __name__)
+Sorin = Symbol("Sorin", __name__)
 Tezzeret = Symbol("Tezzeret", __name__)
 
 all_artifacts = set([Contraption, Equipment, Fortification])
@@ -258,4 +261,4 @@ all_instants = set([Arcane, Trap])
 all_sorceries = set([Arcane])
 all_lands = set([Desert, Forest, Island, Lair, Locus, Mine, Mountain, Plains, PowerPlant, Swamp, Tower, Urzas])
 all_basic_lands = set([Forest, Island, Mountain, Plains, Swamp])
-all_planeswalkers = set([Ajani, Bolas, Chandra, Elspeth, Garruk, Jace, Liliana, Sarkhan, Tezzeret])
+all_planeswalkers = set([Ajani, Bolas, Chandra, Elspeth, Garruk, Jace, Liliana, Nissa, Sarkhan, Sorin, Tezzeret])
