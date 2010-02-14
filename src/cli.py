fbaf135e266fdbb7390d064aa970a5c506a8c31c
Incantus <incantus1@gmail.com>
2010-02-13 22:29:24 -0500

    Memory management debugging in command line interface

--- src/cli.py
+++ src/cli.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 from optparse import OptionParser
-import readline, pudb, pdb
+import readline, pudb, pdb, gc
 import time, random
 from itertools import chain, repeat, izip
 import ConfigParser
@@ -8,6 +8,36 @@ from engine.GameKeeper import Keeper
 from engine.Player import Player
 from engine import Action, Mana
 from network import replaydump
+
+# Recursively expand slist's objects
+# into olist, using seen to track
+# already processed objects.
+def _getr(slist, olist, seen):
+  for e in slist:
+    if id(e) in seen:
+      continue
+    seen[id(e)] = None
+    olist.append(e)
+    tl = gc.get_referents(e)
+    if tl:
+      _getr(tl, olist, seen)
+
+# The public function.
+def get_all_objects():
+  """Return a list of all live Python
+  objects, not including the list itself."""
+  gc.collect()
+  gcl = gc.get_objects()
+  olist = []
+  seen = {}
+  # Just in case:
+  seen[id(gcl)] = None
+  seen[id(olist)] = None
+  seen[id(seen)] = None
+  # _getr does the real work.
+  _getr(gcl, olist, seen)
+  return olist
+
 
 debug = pdb
 
@@ -52,12 +82,13 @@ def text_input(msg):
         text = raw_input(printer.prefix+msg)
         if (text and text[0] == '!'): # Helper functions
             if text == "!d": debug.set_trace()
-            if text[:2] == "!c":
+            elif text[:2] == "!c":
                 try:
                     card = card_map.get(int(text[2:]), None)
                     if card: printer("%s"%card.info)
                     else: printer("Invalid card")
                 except: pass
+            elif text == "!g": print gc.collect()
         else: break
     return text
 
